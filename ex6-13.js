89b3c99710622a120d60ca288d24cae266d9f159
kosukeyoshida1262 <zm19453@josai.ac.jp>
2022-01-18 01:51:11 +0000

    0118

--- ex6-13.js
+++ ex6-13.js
@@ -1,2 +1,1 @@
-function imgchange3() {document.getElementById('logo3').src='./image/josai.logo.png';}
-function bthrees() {document.getElementById('logo3').src='./image/keiei_small.jpg';}
+function img() {document.getElementById('logo3').src='./image/keiei_small.jpg';}
